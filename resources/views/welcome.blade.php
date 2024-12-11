b07035b9a067d96a6ee3d6edb5df7990b82b20c9
Rui Martins <armm100@hotmail.com>
2024-12-11 00:20:03 +0000

    #03 - add logo and adjust layout navbar and your behaviour

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,28 +27,29 @@
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 lg:grid-cols-2">
-                        <div class="flex ">
-                            <img id="logo" class="max-h-28" src="{{ Vite::asset('resources/images/logo_fune.png') }}" alt="Homenagem Final" />
+                        <div class="flex items-center">
+                            <img id="logo" class="max-h-24" src="{{ Vite::asset('resources/images/logo_fune.png') }}" alt="Homenagem Final" />
                             <div class="flex justify-center items-center">
                                 <a href="/" class="block font-semibold text-2xl text-gray-900 ml-4">
                                     Homenagem Final
                                 </a>
                             </div>
-
                         </div>
                         @if (Route::has('login'))
-                            <nav class="-mx-3 flex flex-1 justify-end">
+                            <nav class="-mx-3 flex flex-1 justify-end items-start h-full">
                                 @auth
                                     <a
                                         href="{{ url('/dashboard') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        class="bg-blue-400 h-full px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        style="max-width: 100px; width: 100%; height: auto;"
                                     >
                                         Conta
                                     </a>
                                 @else
                                     <a
                                         href="{{ route('login') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        class="bg-blue-400 h-full px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        style="max-width: 100px; width: 100%; height: auto;"
                                     >
                                         Log in
                                     </a>
@@ -56,6 +57,7 @@
                             </nav>
                         @endif
                     </header>
+
 
                     <main class="mt-6">
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
